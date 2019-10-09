effd342e895a9f6c5c950c21e12a07c7c8513dd7
Natalia <n.platova@hotmail.com>
2019-10-09 21:43:22 +0700

    6

---
+++ vacancy/src/components/filter/filter.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import './filter.css';
+
+class Filter extends React.Component{
+    constructor(props) {
+        super(props);
+        this.holdChange = this.holdChange.bind(this);
+    }
+
+    holdChange(e) {
+        const newQueryParams = e.target.id;
+        const newWordQuery = e.target.value;
+        this.props.onChangeParams(newQueryParams, newWordQuery);
+    }
+
+    render() {
+        return(
+            <div className='filter'>
+                <div className="form-check">
+                    <input className="form-check-input" type="checkbox" value="true" id="full_time=" onChange={this.holdChange}/>
+                        <label className="form-check-label" htmlFor="full_time=">
+                            Full time
+                        </label>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="location=">Location</label>
+                    <input className="form-control" id="location=" onMouseLeave={this.holdChange}/>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="description=">Description</label>
+                    <input className="form-control" id="description=" onMouseLeave={this.holdChange}/>
+                </div>
+            </div>
+
+        );
+    }
+}
+
+export default Filter;
