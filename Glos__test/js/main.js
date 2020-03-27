e43de6cc6c9bdd07696122cdd9553c03dce3c014
Natalia <n.platova@hotmail.com>
2020-03-27 14:20:49 +0700

    corrected js in glos test

--- Glos__test/js/main.js
+++ Glos__test/js/main.js
@@ -13,12 +13,14 @@ $(document).ready(function(){
 
     $('.feedback-button').on('click', function() {
         $('.feedback__overlay').css('display', 'flex');
+        $('html').css('overflow', 'hidden');
     });
 
     $('.feedback__close').on('click', function () {
         $('.feedback__overlay').css('display', 'none');
+        $('html').css('overflow', 'unset');
     });
-    
+
     $('.add-to-basket').on('click', function () {
         alert('Товар добавлен в корзину');
     });
